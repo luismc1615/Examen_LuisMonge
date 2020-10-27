11141f0181f1098b5f3a4d25df3e6edddbd25625
luismc1615 <58350003+luismc1615@users.noreply.github.com>
2020-10-26 22:57:00 -0600

    Inicio del API del examen

---
+++ exa_lmc/src/main/java/org/una/exa_lmc/ExaLmcApplication.java
@@ -0,0 +1,13 @@
+package org.una.exa_lmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaLmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaLmcApplication.class, args);
+	}
+
+}
---
+++ exa_lmc/src/test/java/org/una/exa_lmc/ExaLmcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.una.exa_lmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaLmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
